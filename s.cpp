a80b316bb478af01e293d52f70504b473d02e71e
yahay9356-netizen <yahay9356@gmail.com>
2026-04-24 04:05:13 +0300

    7

---
+++ s.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class first
+{
+private:
+    int x;
+public:
+void print(){
+    cout << x << endl;
+}
+};
+class second : private first
+{
+private:
+    int b;
+
+// public:
+//     void set()
+//     {
+//         cout << "Enter x" << endl;
+//         cin >> x;
+//     }
+};
+int main()
+{
+    first op1;
+    op1.print();
+}
